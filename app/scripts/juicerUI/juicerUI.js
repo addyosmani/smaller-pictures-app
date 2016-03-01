e594971eadd1b3c7e394beaa1295d2e0cff2034a
Addy Osmani <addyosmani@gmail.com>
2016-03-01 18:32:43 +0000

    Add easter egg

--- app/scripts/juicerUI/juicerUI.js
+++ app/scripts/juicerUI/juicerUI.js
@@ -64,6 +64,9 @@ class juicerUI extends utils {
         // Initialise events
         this.setupEvents();
 
+        // Run Easter Egg
+        this.runEasterEgg();
+
       });
     }
 
@@ -257,5 +260,11 @@ class juicerUI extends utils {
       this.l.addEventListener('change', (e) => {
         this.orchestrateEncode(this.l);
       });
+    }
+
+    runEasterEgg() {
+      // DevTools styled console log.
+      const cssStyle = "font-size: 200px; color: hsl(330, 100%, 50%); text-shadow: 0 2px 0 hsl(330, 100%, 25%), 0 3px 2px hsla(330, 100%, 15%, 0.5), /* next */ 0 3px 0 hsl(350, 100%, 50%), 0 5px 0 hsl(350, 100%, 25%), 0 6px 2px hsla(350, 100%, 15%, 0.5), /* next */ 0 6px 0 hsl(20, 100%, 50%), 0 8px 0 hsl(20, 100%, 25%), 0 9px 2px hsla(20, 100%, 15%, 0.5), /* next */ 0 9px 0 hsl(50, 100%, 50%), 0 11px 0 hsl(50, 100%, 25%), 0 12px 2px hsla(50, 100%, 15%, 0.5), /* next */ 0 12px 0 hsl(70, 100%, 50%), 0 14px 0 hsl(70, 100%, 25%), 0 15px 2px hsla(70, 100%, 15%, 0.5), /* next */ 0 15px 0 hsl(90, 100%, 50%), 0 17px 0 hsl(90, 100%, 25%), 0 17px 2px hsla(90, 100%, 15%, 0.5); font-family: 'Mr Dafoe'";
+      console.log("%c yolo!", cssStyle);
     }
 }

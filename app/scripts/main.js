7ae0201cb25174692d8c560effe02dd2722dde10
Addy Osmani <addyosmani@gmail.com>
2016-03-11 15:06:56 +0000

    Version bump.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@
 (function() {
   'use strict';
 
-  var APP_VERSION = 1.0238;
+  var APP_VERSION = 1.0239;
   console.log('Running Smaller Pics version ' + APP_VERSION);
 
   // Check to make sure service workers are supported in the current browser,
